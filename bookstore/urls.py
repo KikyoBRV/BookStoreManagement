142f8a642e43ffb8ee919b009fc503b4ddc3c88a
Buravit Yenjit <khiaokrub@gmail.com>
2024-11-26 12:20:20 +0700

    Create plain template for each page

--- bookstore/urls.py
+++ bookstore/urls.py
@@ -4,5 +4,9 @@ from . import views
 app_name = 'bookstore'
 
 urlpatterns = [
-    path('', views.HomeView.as_view(), name='home'),  # Home page
+    path('', views.HomeView.as_view(), name='home'),
+    path('inventory/', views.InventoryView.as_view(), name='inventory'),
+    path('sales/', views.SalesView.as_view(), name='sales'),
+    path('customer/', views.CustomerView.as_view(), name='customer'),
+    path('supplier/', views.SupplierView.as_view(), name='supplier'),
 ]
--- bookstore/views.py
+++ bookstore/views.py
@@ -1,4 +1,35 @@
 from django.views.generic import TemplateView
 
 class HomeView(TemplateView):
-    template_name = 'home.html'
+    """
+    Renders the home page with navigation to other sections.
+    """
+    template_name = "home.html"
+
+
+class InventoryView(TemplateView):
+    """
+    Renders the inventory management page.
+    """
+    template_name = "inventory.html"
+
+
+class SalesView(TemplateView):
+    """
+    Renders the sales processing page.
+    """
+    template_name = "sales.html"
+
+
+class CustomerView(TemplateView):
+    """
+    Renders the customer management page.
+    """
+    template_name = "customers.html"
+
+
+class SupplierView(TemplateView):
+    """
+    Renders the supplier and procurement management page.
+    """
+    template_name = "supplier.html"
