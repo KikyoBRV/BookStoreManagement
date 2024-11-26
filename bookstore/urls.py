813563ee7126ac626fe0d84dc6d9b2906a0de415
Buravit Yenjit <khiaokrub@gmail.com>
2024-11-26 14:20:18 +0700

    Update Customer management system

--- bookstore/urls.py
+++ bookstore/urls.py
@@ -10,5 +10,8 @@ urlpatterns = [
     path('inventory/', views.InventoryView.as_view(), name='inventory'),
     path('sales/', views.SalesView.as_view(), name='sales'),
     path('customer/', views.CustomerView.as_view(), name='customer'),
+    path('customer/create/', views.CustomerCreateView.as_view(), name='customer_create'),
+    path('customer/<int:pk>/edit/', views.CustomerUpdateView.as_view(), name='customer_edit'),
+    path('customer/<int:pk>/delete/', views.CustomerDeleteView.as_view(), name='customer_delete'),
     path('supplier/', views.SupplierView.as_view(), name='supplier'),
 ]
--- bookstore/views.py
+++ bookstore/views.py
@@ -1,7 +1,10 @@
-from django.views.generic import TemplateView
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import get_object_or_404
 from django.urls import reverse_lazy
-from django.views.generic.edit import CreateView
+from django.views.generic import TemplateView, CreateView, UpdateView, DeleteView
+from .models import Customer
+
 
 class SignUpView(CreateView):
     template_name = 'registration/signup.html'
@@ -29,12 +32,58 @@ class SalesView(TemplateView):
     template_name = "sales.html"
 
 
-class CustomerView(TemplateView):
+class CustomerView(LoginRequiredMixin, TemplateView):
     """
-    Renders the customer management page.
+    Renders the customer management page, showing only the customers for the logged-in user.
     """
     template_name = "customers.html"
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # Fetch only the customers related to the logged-in user
+        context['customers'] = Customer.objects.filter(user=self.request.user)
+        return context
+
+class CustomerCreateView(LoginRequiredMixin, CreateView):
+    model = Customer
+    fields = ['phone', 'loyalty_points']
+    template_name = 'customer_form.html'
+
+    def form_valid(self, form):
+        # Associate the new customer with the currently logged-in user
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        # Redirect to the customer management page after successful creation
+        return reverse_lazy('bookstore:customer')
+
+class CustomerUpdateView(LoginRequiredMixin, UpdateView):
+    model = Customer
+    fields = ['phone', 'loyalty_points']
+    template_name = 'customer_form.html'
+
+    def get_object(self, queryset=None):
+        # Ensure that the customer being edited belongs to the current user
+        customer = get_object_or_404(Customer, pk=self.kwargs['pk'], user=self.request.user)
+        return customer
+
+    def get_success_url(self):
+        # Redirect to the customer management page after successful update
+        return reverse_lazy('bookstore:customer')
+
+class CustomerDeleteView(LoginRequiredMixin, DeleteView):
+    model = Customer
+    template_name = 'customer_confirm_delete.html'
+
+    def get_object(self, queryset=None):
+        # Ensure that the customer being deleted belongs to the current user
+        customer = get_object_or_404(Customer, pk=self.kwargs['pk'], user=self.request.user)
+        return customer
+
+    def get_success_url(self):
+        # Redirect to the customer management page after successful deletion
+        return reverse_lazy('bookstore:customer')
 
 class SupplierView(TemplateView):
     """
